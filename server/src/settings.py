eab89eab5ead9f447ad988a75f82aeec54ab9da0
Jungjihyuk <wlgurdlcjswo@naver.com>
2021-07-14 22:16:24 +0900

    chore: mongodb 연동

--- server/src/settings.py
+++ server/src/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -74,10 +75,14 @@ WSGI_APPLICATION = 'src.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'default': {
+            'ENGINE': 'djongo',
+            'NAME': 'twigerDB',
+            'ENFORCE_SCHEMA': False,
+            'CLIENT': {
+                'host': 'mongodb+srv://jihyeok:jjh8493@twigerdb.oyqtu.mongodb.net/twigerDB?retryWrites=true&w=majority'
+            }  
+        }
 }
 
 
