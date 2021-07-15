414e2b7a8c432c5f497068c0e03c34850953c441
Jungjihyuk <wlgurdlcjswo@naver.com>
2021-07-15 20:16:49 +0900

    chore: secret file 경로 수정

--- server/src/settings.py
+++ server/src/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-secret_file = os.path.join(BASE_DIR, 'secrets.json')
+secret_file = os.path.join(BASE_DIR, 'json/secrets.json')
 
 
 with open(secret_file) as f:
