a06a9f138fca2f9cca2e4d37600eb914968c5915
Jungjihyuk <wlgurdlcjswo@naver.com>
2021-07-18 22:10:09 +0900

    refactor: cors_allowed_origins 리스트로 받아오도록 수정

--- server/src/settings.py
+++ server/src/settings.py
@@ -149,8 +149,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = [
-    get_key('Client_Resource')  
-]
+CORS_ALLOWED_ORIGINS = get_key('Client_Resource')  
+
 
 APPEND_SLASH = False
