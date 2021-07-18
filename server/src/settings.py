391cdb4ca0c34d6410c0fd610b6e3adb01ef0e83
Jungjihyuk <wlgurdlcjswo@naver.com>
2021-07-18 23:20:24 +0900

    chore: 주석 삭제

--- server/src/settings.py
+++ server/src/settings.py
@@ -17,14 +17,6 @@ from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-
-# SECURITY WARNING: don't run with debug turned on in production!
 secret_file = os.path.join(BASE_DIR, 'json/secrets.json')
 
 
