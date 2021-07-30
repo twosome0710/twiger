976304991c2ecbdf6429d0f857e1a6ff51637598
Jungjihyuk <wlgurdlcjswo@naver.com>
2021-07-30 15:35:01 +0900

    refactor: 회원 가입 API 수정

--- server/users/views.py
+++ server/users/views.py
@@ -1,42 +1,34 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.http import HttpResponse
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import UserSerializer
-
-from users.models import User
+from .models import Users
 
 from pymongo import MongoClient
-from src.settings import get_key
+from config.settings import get_key
 import datetime 
+import certifi
  
-conn = MongoClient('mongodb+srv://'+get_key("MongoDB_UserName")+':'+get_key("MongoDB_PWD")+'@twigerdb.oyqtu.mongodb.net/'+get_key("MongoDB_DBName")+'?retryWrites=true&w=majority')
+conn = MongoClient(get_key("Driver_Code"), tlsCAFile=certifi.where())
 db = conn.get_database(get_key("MongoDB_DBName"))
-collection = db.twigerDB.User
-
-print('conn: ', conn)
-
-@api_view(['GET'])
-def apiOverview(request):
-    api_urls = {
-        'SignUp' : '/',
-    }
-
-    return Response(api_urls)
+collection = db.Users
 
 
 @api_view(['POST'])
 def signUp(request):
     serializer = UserSerializer(data = request.data)
-
+    
     if serializer.is_valid():
-        serializer.save()
+        if list(collection.find({"email": serializer.data["email"]})): 
+            return Response({"message": "사용중인 이메일 입니다."}, status=status.HTTP_400_BAD_REQUEST)
+
         collection.insert_one(serializer.data)
-        print("Sign up success")
+        pid = list(collection.find())[::-1][0]['_id']
         
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response({"pid": str(pid), "messge": "Sign up success"}, status=status.HTTP_201_CREATED)
     else:
-        return Response(serializer._errors, status=status.HTTP_400_BAD_REQUEST)
-        
+        return Response({"message": "유효한 이메일 주소를 입력하시오."}, status=status.HTTP_400_BAD_REQUEST)
