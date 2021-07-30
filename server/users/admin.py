5d85165e5b95042d2c3ce13856adfb48d2aa90ba
Jungjihyuk <wlgurdlcjswo@naver.com>
2021-07-30 15:35:01 +0900

    refactor: 회원 가입 API 수정

--- server/users/admin.py
+++ server/users/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from users.models import Users
 
-from users.models import User
-
-admin.site.register(User)
+admin.site.register(Users)
--- server/users/models.py
+++ server/users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-class User(models.Model):
-    email = models.EmailField(max_length=254, verbose_name="이메일")
+class Users(models.Model):
+    email = models.EmailField(max_length=254, unique=True, verbose_name="이메일")
     password = models.CharField(max_length=50, verbose_name="비밀번호")
     name = models.CharField(max_length=50, verbose_name="이름")
     birth = models.DateField(auto_now=False, auto_now_add=False)
