a56f9bc929d163a62e3279e0d8affb005b4b5aad
jinkwan.kim <kkole3897@naver.com>
2021-09-08 05:39:57 +0900

    feat: SidMenu 스크롤 안되도록 고정

--- client/src/components/common/SideMenu.vue
+++ client/src/components/common/SideMenu.vue
@@ -1,67 +1,71 @@
 <template>
   <header class="main_header">
-    <h1 class="title">
-      <router-link to="/home" class="link">
-        <img
-          src="@/assets/images/logo_blue.png"
-          alt="트위저"
-          width="30"
-          class="twiger_logo"
-        />
-      </router-link>
-    </h1>
-    <nav class="nav_area">
-      <div class="link_wrap">
-        <router-link
-          to="/home"
-          class="link"
-          :class="{ selected: isCurRouteName('UserHome') }"
-        >
-          <div class="container">
-            <IconHomeSelected class="icon_home_selected" />
-            <IconHome class="icon_home" />
-            홈
-          </div>
-        </router-link>
-      </div>
-      <div class="link_wrap">
-        <router-link
-          to="#"
-          class="link"
-          :class="{ selected: isCurRouteName('Explore') }"
-        >
-          <div class="container"><IconSharp class="icon_sharp" />탐색하기</div>
-        </router-link>
-      </div>
-      <div class="link_wrap">
-        <router-link
-          to="#"
-          class="link"
-          :class="{ selected: isCurRouteName('Profile') }"
-        >
-          <div class="container">
-            <IconProfile class="icon_profile" />프로필
-          </div>
-        </router-link>
-      </div>
-    </nav>
-    <TweetButton type="button" label="트윗하기" class="btn_tweet" />
-    <div class="account_area">
-      <button type="button" class="btn_account">
-        <div class="profile_area">
+    <div class="fixed_container">
+      <h1 class="title">
+        <router-link to="/home" class="link">
           <img
-            src="@/assets/images/default_profile.png"
-            alt=""
-            width="40"
-            class="img_profile"
+            src="@/assets/images/logo_blue.png"
+            alt="트위저"
+            width="30"
+            class="twiger_logo"
           />
+        </router-link>
+      </h1>
+      <nav class="nav_area">
+        <div class="link_wrap">
+          <router-link
+            to="/home"
+            class="link"
+            :class="{ selected: isCurRouteName('UserHome') }"
+          >
+            <div class="container">
+              <IconHomeSelected class="icon_home_selected" />
+              <IconHome class="icon_home" />
+              홈
+            </div>
+          </router-link>
         </div>
-        <div class="name_area">
-          <span class="text_name">유저 이름</span>
-          <span class="text_id">@이메일 앞부분</span>
+        <div class="link_wrap">
+          <router-link
+            to="#"
+            class="link"
+            :class="{ selected: isCurRouteName('Explore') }"
+          >
+            <div class="container">
+              <IconSharp class="icon_sharp" />탐색하기
+            </div>
+          </router-link>
         </div>
-        <IconDots class="icon_more" />
-      </button>
+        <div class="link_wrap">
+          <router-link
+            to="#"
+            class="link"
+            :class="{ selected: isCurRouteName('Profile') }"
+          >
+            <div class="container">
+              <IconProfile class="icon_profile" />프로필
+            </div>
+          </router-link>
+        </div>
+      </nav>
+      <TweetButton type="button" label="트윗하기" class="btn_tweet" />
+      <div class="account_area">
+        <button type="button" class="btn_account">
+          <div class="profile_area">
+            <img
+              src="@/assets/images/default_profile.png"
+              alt=""
+              width="40"
+              class="img_profile"
+            />
+          </div>
+          <div class="name_area">
+            <span class="text_name">유저 이름</span>
+            <span class="text_id">@이메일 앞부분</span>
+          </div>
+          <IconDots class="icon_more" />
+        </button>
+      </div>
     </div>
   </header>
 </template>
@@ -107,6 +111,12 @@ export default {
   width: 251px;
   height: 100%;
   padding: 0 12px;
+
+  .fixed_container {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+  }
 
   .title {
     padding: 2px 0;
